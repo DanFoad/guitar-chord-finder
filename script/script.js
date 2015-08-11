6d1f74ac055fdeeca90d5bc36b8ed8ff80a4f8bc
Dan Foad <danielfoad@gmail.com>
2015-08-11 16:15:17 +0000

    Input functionality

--- script/script.js
+++ script/script.js
@@ -3,3 +3,15 @@ $(".finder__input").focus(function() {
 }).mouseup(function(e) {
     e.preventDefault();
 });
+
+$(".finder__input").keyup(function() {
+    if ($(this).val().length > 0) {
+        var name = $(this).attr("name");
+        if (name == "f6") {
+            $(".finder__inputs input[type=submit]").focus();
+        } else {
+            n = parseInt(name.replace("f", ""));
+            $(".finder__input[name=f" + n++ + "]").focus();
+        }
+    }
+});
